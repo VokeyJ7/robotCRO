d587eb79deb1b0a5791459dbc698f46ab2f31752
Veltman Jr <161249791+VokeyJ7@users.noreply.github.com>
2026-06-02 13:00:14 -0500

    Refactor control.py for improved readability
    Refactor code for readability

--- control.py
+++ control.py
@@ -4,18 +4,18 @@ import tty
 import termios
 from pynput import keyboard
 
-# arduino 2 is the motor controller
+
 arduino2 = serial.Serial("/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_7583033333835120C1C2-if00", 9600, timeout=1)
 
 def get_key():
-	fd = sys.stdin.fileno()# gets the file descriptor
-	old= termios.tcgetattr(fd) # saves the current terminal settings before anything changes 
+	fd = sys.stdin.fileno()
+	old= termios.tcgetattr(fd) 
 	try:
-		tty.setraw(fd)# sets the terminal to raw mode and eliminates any buffering so all inputs are sent right away
-		key = sys.stdin.read(1) # reads exactly one character input and records it
+		tty.setraw(fd)
+		key = sys.stdin.read(1) 
 	finally:
-		termios.tcsetattr(fd, termios.TCSADRAIN, old) #restores the terminal back to the normal settings saved in "old" 
-		# TDSADRAIN means wait until all output is finished before restoring 
+		termios.tcsetattr(fd, termios.TCSADRAIN, old) 
+		
 	return key
 
 while True:
