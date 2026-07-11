16d42ae9f39b02001c13489277f179637ef9f155
Veltman Jr <161249791+VokeyJ7@users.noreply.github.com>
2026-07-11 16:13:00 -0500

    Clean up in llm.py

--- llm.py
+++ llm.py
@@ -17,11 +17,7 @@ import time
 from control import get_key
 from camera import capture
 
-# terminal freeze error came from gemini client failing silently under the hood. 
-# the error wouldn't stop the program the program continues to the control loop so that is why you could still control the car with no response from gemini
-# to confirm that program is working press 'e' if the program is not terminated that means gemini is still initializing
-# the print statements of the model initialization will help me know what state the program is in
-# turns out it wasn't the api that was slow it might have been subprocess and the imports
+
 
 
 load_dotenv(dotenv_path="gemini.env")
@@ -31,7 +27,7 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 system_prompt = "You are the brain of a robot that I am constructing. I will proide you prompts. Return your answer immediately. Be concise and say exactly what I tell you to. Introduce yourself as CRO (Car Robot Operator) a Gemini AI Agent for this Car Robot before you state your response to my prompt."
 
-# arduino 2 is the motor controller, remember its serial address
+
 
 arduino2 = serial.Serial("/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_7583033333835120C1C2-if00", 9600, timeout=1)
 
